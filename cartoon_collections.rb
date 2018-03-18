e3a8128faa6b5424db0be7137551e59c77bf3293
Greg Stimson <stimsong@yahoo.com>
2018-03-18 19:03:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,7 @@ end
 def summon_captain_planet(veggies)
 #  alt_veggies = []
 #  alt_veggies <<
-  veggies.collect { |veg|
-    veg.capitalize }
+  veggies.collect { |veg| veg.capitalize + "!" }
 #  alt_veggies
 end
 
