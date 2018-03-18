454be5bfda6aae47faad6ce6b7fb0f37cab34ecb
Greg Stimson <stimsong@yahoo.com>
2018-03-18 19:32:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,11 +8,12 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls(calls)
-
   calls.any? { |call_length| call_length.size > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(recipe)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  recipe.include?(cheese_types)
 end
