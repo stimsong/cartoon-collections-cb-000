e88dbcc4710db9b8676d26e42035e280eea8a845
Greg Stimson <stimsong@yahoo.com>
2018-03-18 19:04:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,10 @@
 def roll_call_dwarves(dwarves)
-
   dwarves.each_with_index { | dwarf, index |
     puts "#{index+1} #{dwarf}" }
 end
 
 def summon_captain_planet(veggies)
-#  alt_veggies = []
-#  alt_veggies <<
   veggies.collect { |veg| veg.capitalize + "!" }
-#  alt_veggies
 end
 
 def long_planeteer_calls# code an argument here
