e36b729597ce1013e02ccfd3bf000fab667ade5c
Greg Stimson <stimsong@yahoo.com>
2018-03-18 19:39:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,6 @@ def find_the_cheese(recipe)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  cheese_types.collect { |c| recipe.include?(c) }
+  cheese_types.collect { |c| recipe.include?(c) }.include?(true)
+
 end
